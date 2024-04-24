d1c53f16f9a25d9a3d2560c2d07de9d935378915
Steve Wang <stevewang@alumni.stanford.edu>
2024-04-24 17:09:05 -0400

    Add GameObjectBlueprint classes

---
+++ BlueprintObjectParser.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "GameObjectBlueprint.h"
+#include <string>
+
+using namespace std;
+
+class BlueprintObjectParser
+{
+    private:
+        string extractStringBetweenTags(
+            string stringToSearch,
+            string startTag,
+            string endTag
+        );
+
+    public:
+        void parseNextObjectForBlueprint(ifstream& reader, GameObjectBlueprint& bp);
+};
