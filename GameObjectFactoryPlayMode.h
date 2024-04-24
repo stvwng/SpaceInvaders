7b5d61b57fee5ffbc6f08867dcce27aef2b6aea0
Steve Wang <stevewang@alumni.stanford.edu>
2024-04-24 17:54:11 -0400

    Add GameObjectFactoryPlayMode class

---
+++ GameObjectFactoryPlayMode.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameObjectBlueprint.h"
+#include "GameObject.h"
+#include <vector>
+
+class GameObjectFactoryPlayMode
+{
+    public:
+        void buildGameObject(
+            GameObjectBlueprint& bp,
+            std::vector<GameObject>& gameObjects
+        );
+};
